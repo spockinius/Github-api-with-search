4eb388df0471c4213ed46d825a9a256e2b79439e
Katariina Vakkuri <katariina.vakkuri@polarsquad.com>
2022-09-28 13:16:16 +0300

    second push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/search/repositories?q=react')
+    .then(response => response.json())
+    .then(resData => setItems(resData.data))
+    .catch(err => console.log(err))
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>Repositories</h1>
+      <table>
+        <tbody>
+        {
+          items.map((item, index) => 
+           <tr key={index}>
+             <td>{item.id}</td>
+           </tr>
+          )
+        }
+        </tbody>
+      </table>
+     </div>
   );
 }
 
 export default App;
+
