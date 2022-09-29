3e5602e524cb7375f05cc2a9000f35110f376b95
Katariina Vakkuri <katariina.vakkuri@polarsquad.com>
2022-09-29 18:06:22 +0300

    first push

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 function App() {
   const [items, setItems] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
 
   useEffect(() => {
     fetch('https://api.github.com/search/repositories?q=react')
@@ -11,10 +12,28 @@ function App() {
     .catch(err => console.log(err))
   }, [])
 
+  const fetchData = () => {
+    const url = ('https://api.github.com/search/repositories?q=' + searchInput)
+    console.log(url)
+    fetch(url)
+    .then(response => response.json())
+    .then(resData => setItems(resData.items))
+    .catch(err => console.log(err))
+  };
+  
+  const handleChange = (e) => {
+    e.preventDefault();
+    setSearchInput(e.target.value);
+  };
+
   return (
     <div className="App">
       <h1>Repositories</h1>
-      <table class="repos">
+      <form>
+      <input type="search" placeholder="Search" onChange={handleChange} value={searchInput} />
+      <button type="button" onClick={fetchData}>Search</button>
+      </form>
+      <table className="repos">
         <thead>
         <tr>
           <th>Name</th>
